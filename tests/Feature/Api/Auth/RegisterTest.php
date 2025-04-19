d466e53041862c3d9c90516710763228e73339ca
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-04-19 05:34:26 -0300

    test: it should return email already exists error

--- tests/Feature/Api/Auth/RegisterTest.php
+++ tests/Feature/Api/Auth/RegisterTest.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use App\Models\User;
 use Illuminate\Http\Response;
 
 it('should be able to register a user', function () {
@@ -46,4 +47,29 @@ it('should return a 422 error when the password is not confirmed', function () {
             ],
         ],
     ]);
+});
+
+it('should return email already exists error', function () {
+    User::factory()->create([
+        'email' => 'test@example.com',
+    ]);
+
+    $payload = [
+        'name' => 'Test User',
+        'email' => 'test@example.com',
+        'password' => 'P@ssw0rd',
+        'password_confirmation' => 'P@ssw0rd',
+    ];
+
+    $response = $this->postJson(route('api.auth.register'), $payload);
+    $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+    $response->assertJson([
+        'errors' => [
+            'email' => [
+                'The email has already been taken.',
+            ],
+        ],
+    ]);
+
+    $this->assertDatabaseCount('users', 1);
 });
