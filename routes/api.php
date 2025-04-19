5c61b04663ea350a14b78bd90a5217bd0beb3d0e
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-04-19 05:50:04 -0300

    fix: fix route prefix

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('api')->name('api.')->group(function () {
+Route::name('api.')->group(function () {
     Route::prefix('auth')->name('auth.')->group(function () {
         Route::post('register', [AuthController::class, 'register'])->name('register');
     });
