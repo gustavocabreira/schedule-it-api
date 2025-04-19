4a057e2c2efa22b4c96b0de96d69831f1f6e3e28
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-04-19 05:36:07 -0300

    test: should return a 422 error when the email is not valid

--- tests/Feature/Api/Auth/RegisterTest.php
+++ tests/Feature/Api/Auth/RegisterTest.php
@@ -73,3 +73,23 @@ it('should return email already exists error', function () {
 
     $this->assertDatabaseCount('users', 1);
 });
+
+it('should return a 422 error when the email is not valid', function () {
+    $payload = [
+        'name' => 'Test User',
+        'email' => 'not valid',
+        'password' => 'P@ssw0rd',
+        'password_confirmation' => 'P@ssw0rd',
+    ];
+
+    $response = $this->postJson(route('api.auth.register'), $payload);
+
+    $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+    $response->assertJson([
+        'errors' => [
+            'email' => [
+                'The email field must be a valid email address.',
+            ],
+        ],
+    ]);
+});
