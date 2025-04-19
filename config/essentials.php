0434c1c5fd1b8c599a25baee66fb51c5383c6c80
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-04-19 05:21:23 -0300

    chore: configure essentials

--- config/essentials.php
+++ config/essentials.php
@@ -4,4 +4,7 @@ declare(strict_types=1);
 
 return [
     NunoMaduro\Essentials\Configurables\ShouldBeStrict::class => true,
+    NunoMaduro\Essentials\Configurables\ForceScheme::class => true,
+    NunoMaduro\Essentials\Configurables\ImmutableDates::class => true,
+    NunoMaduro\Essentials\Configurables\ProhibitDestructiveCommands::class => true,
 ];
